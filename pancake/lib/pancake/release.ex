7f8a24e55777899cb932998a8df0c632c003d351
firecraftgaming <eliyah@sundstroem.com>
2021-06-06 23:44:46 +0200

    feat(pancake): added docker support

---
+++ pancake/lib/pancake/release.ex
@@ -0,0 +1,16 @@
+defmodule Pancake.Release do
+  @app :pancake
+
+  def migrate do
+    load_app()
+  end
+
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp load_app do
+    Application.load(@app)
+  end
+end
